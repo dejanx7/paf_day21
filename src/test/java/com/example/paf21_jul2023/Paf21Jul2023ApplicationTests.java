aa2d97f5aa177eb73bba60d8c7bb3e324842ac51
dejanx7 <arunnarenderan1@gmail.com>
2023-06-20 12:27:22 +0800

    first commit

---
+++ src/test/java/com/example/paf21_jul2023/Paf21Jul2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paf21_jul2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf21Jul2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
